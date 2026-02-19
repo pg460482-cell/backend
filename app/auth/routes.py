03e1acb4a9f467190b0bd09414df9c9547c88c83
pg460482-cell <pg460482@gmail.com>
2026-02-19 19:16:35 +0530

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -14,7 +14,7 @@ from sqlalchemy import or_
 import re
 def validate_email(email):
     pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-    return re.match([pattern,email])is not None
+    return re.match(pattern,email)is not None
 def validate_password(password):
     if len(password)<8:
         return False,"Password must be at least 8 characters long"
