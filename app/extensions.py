90b4369d3a2756185693427c9cea31229aff8892
pg460482-cell <pg460482@gmail.com>
2026-02-21 04:15:52 +0530

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -23,6 +23,7 @@ from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask import jsonify
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
